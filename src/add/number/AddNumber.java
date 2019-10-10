20f3739470c74886647d1c754a535284a922a71d
dulmi1228 <48253729+dulmi1228@users.noreply.github.com>
2019-10-10 18:46:59 +0530

    javanew4

---
+++ src/add/number/AddNumber.java
@@ -0,0 +1,17 @@
+
+package add.number;
+
+
+public class AddNumber {
+
+   
+    public static void main(String[] args) {
+         int first = 10;
+        int second = 20;
+
+        int sum = first + second;
+
+        System.out.println("The sum is: " + sum);
+    }
+    
+}
